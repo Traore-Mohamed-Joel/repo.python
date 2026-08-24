51db688e0e406f58586e1500611229211840f98f
mohamed_joel <mohamedjoeltraore@gmail.com>
2026-08-24 13:36:00 +0000

    modification du 24/08

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
-"""Premier programme 
-Formation Python
-apprendre la programmation
-nom = input("quel est votre nom ? ")
-age = input("quel est votre ages ? ")
+# afficher_informations_personne
+# Paramètres : nom, age
+def afficher_informations_personne(nom, age):
+    print()
+    print("Vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
+    print("L'an prochain vous aurez " + str(age + 1) + " ans")
 
-try:
-    age_prochain = int(age) + 1
-except ValueError: 
-    print("ERREUR: Vous devez rentrer un nombre pour l'age")
-else:
-    print("vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
-    print("l'an prochain vous aurez " + str(age_prochain) + " ans")"""
+    condition = age >= 18
+    print(condition)
+    if condition:
+        print("Vous êtes majeur")
+    else:
+        print("Vous êtes mineur")
 
-n = 0
-# print(n)
-# n = 1
-# print(n)
-# n = n + 1
-# print(n)
 
-# while n < 4:
-#     print("Valeur de n: " + str(n))
-#     n = n + 1
-# print("fin de la boucle")
+def demander_nom():
+    reponse_nom = ""
+    while reponse_nom == "":
+        reponse_nom = input("Quel est votre nom ? ")
+    return reponse_nom
 
-mot_de_passe = ""
-while not mot_de_passe == "TOTO" :
-    mot_de_passe = input("quel est le mot de passe ? ")
 
-print("Mot de passe correct, vous avez accés au compte")
+def demander_age(nom_personne):
+    age_int = 0
+
+    while age_int == 0:
+        age_str = input(nom_personne + ", quel est votre âge ? ")
+
+        try:
+            age_int = int(age_str)
+        except:
+            print("ERREUR : Vous devez rentrer un nombre pour l'âge")
+
+    return age_int
+
+
+# demander le nom
+nom1 = demander_nom()
+nom2 = demander_nom()
+
+# demander l'âge
+age1 = demander_age(nom1)
+age2 = demander_age(nom2)
+
+# afficher les résultats
+afficher_informations_personne(nom1, age1)
+afficher_informations_personne(nom2, age2)
---
+++ mot_de_passe.py
@@ -0,0 +1,9 @@
+"""Premier programme
+Formation Python
+apprendre la programmation"""
+
+mot_de_passe = ""
+while not mot_de_passe == "TOTO":
+    mot_de_passe = input("Quel est votre mot de passe ?" )
+
+print("Mot de passe corret, vous avez accèss au compte")
